205ff6ec96522e7d23ea580f0c1bf10ae8cd2666
Emmanuel Garza <emmanuel-gf@hotmail.com>
2021-11-24 10:21:13 -0600

    Home implementado

--- index.php
+++ index.php
@@ -1,11 +1,62 @@
-<?php include 'menu.php';?>
+<?php include 'menu.php'; ?>
 <html>
-  <head>
-    <title></title>
-  </head>
 
-  <body class="main">
-    <p>Hola</p>
-  </body>
+<head>
+    <title>Home</title>
+</head>
+
+<body class="main" style="background-color: rgb(195, 211, 218);">
+    <?php
+        require "./funciones/conecta7.php";
+        $con = conecta();
+        $sql = "SELECT * FROM banners WHERE eliminado = 0 AND status = 1 ORDER BY rand() LIMIT 1";
+        $res = $con->query($sql);
+        $row = $res->fetch_array();
+        $imagen = $row["archivo"];
+    ?>
+    <div class="banner"><img src="../Proyecto_backend/archivos/<?php echo $imagen; ?>"></div>
+    <div class="wrap">
+        <div class="contenedor">
+            <?php
+                $con = conecta();
+                $sql = "SELECT * FROM productos WHERE eliminado = 0 AND status = 1 AND stock > 0
+                ORDER BY rand() LIMIT 6";
+                $res = $con->query($sql);
+
+                $cont = 0;
+                while($row = $res->fetch_array()){
+                    $id = $row["id"];
+                    $nombre = $row["nombre"];
+                    $codigo = $row["codigo"];
+                    $costo = $row["costo"];
+                    $stock = $row["stock"];
+                    $archivo = $row["archivo"];
+            ?>
+                <div class="productoHome">
+                    <div><img class="productoHomeImagen" src="../Proyecto_backend/archivos/<?php echo $archivo?>"></div>
+                    <div class="nombreHome"><?php echo $nombre; ?></div><br>
+                    <?php echo number_format($costo,2); ?><br>
+                    <select id="producto_<?php echo $id; ?>" name="producto_<?php echo $id; ?>">
+                        <option value="0">Selecciona</option>
+                        <option value="1" selected>1</option>
+                        <?php
+                            for($contador=2; $contador <= $stock; $contador++){
+                                echo '<option value='.$contador.'>'.$contador.'</option>';
+                            }
+                        ?>
+                    </select>
+                    <input onclick="agregar(<?php echo $id; ?>);return false;" type="submit" value="Agregar"><br>
+                    <div id="mensaje_<?php echo $id; ?>" style="color:#FF0000; font-size:11px; height: 20px"></div>
+                </div>
+            <?php
+                $cont++;
+                /*if($cont >= 6)
+                    break;*/
+            }
+            ?>
+        </div>
+    </div>
+</body>
+
 </html>
-<?php include 'footer.php';?>
+<?php include 'footer.php'; ?>
--- menu.php
+++ menu.php
@@ -22,16 +22,16 @@
     <div id="wrapMenu">
       <div id="contenedorMenu">
         <div id="menu">
-          <a href="./bienvenido.php" class="menuLink">
+          <a href="./index.php" class="menuLink">
             <div id="opcion1" class="opcionMenu" OnMouseEnter="seleccionMenu(1)" OnMouseLeave="deseleccionMenu(1)">Home</div>
           </a>
-          <a href="productos.php" class="menuLink">
+          <a href="./productos.php" class="menuLink">
             <div id="opcion2" class="opcionMenu" OnMouseEnter="seleccionMenu(2)" OnMouseLeave="deseleccionMenu(2)">Productos</div>
           </a>
-          <a href="contacto.php" class="menuLink">
+          <a href="./contacto.php" class="menuLink">
             <div id="opcion3" class="opcionMenu" OnMouseEnter="seleccionMenu(3)" OnMouseLeave="deseleccionMenu(3)">Contacto</div>
           </a>
-          <a href="carrito.php" class="menuLink">
+          <a href="./carrito01.php" class="menuLink">
             <div id="opcion4" class="opcionMenu" OnMouseEnter="seleccionMenu(4)" OnMouseLeave="deseleccionMenu(4)">Carrito</div>
           </a>
         </div>
