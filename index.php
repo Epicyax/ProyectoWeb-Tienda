3562c794f34541a9cd6aca08649d590d2eb6fd84
Emmanuel Garza <emmanuel-gf@hotmail.com>
2021-11-26 16:22:42 -0600

    Función de ver los detalles de un producto

--- index.php
+++ index.php
@@ -3,6 +3,23 @@
 
 <head>
     <title>Home</title>
+    <script>
+        function agregar(id){
+            var articulos = document.productos.producto_.value;
+            if(articulos == "0")
+                alert("Selecciona una cantidad valida");
+            else{
+                document.productos.method = 'post';
+                document.productos.action = 'tablas.php';
+                document.productos.submit();
+            }
+        }
+        function verProducto(id){
+            document.productos.method = 'post';
+            document.productos.action = './productos_detalle.php?id='+id;
+            document.productos.submit();
+        }
+    </script>
 </head>
 
 <body class="main">
@@ -17,13 +34,13 @@
     <div class="banner"><img src="../Proyecto_backend/archivos/<?php echo $imagen; ?>"></div>
     <div class="wrap">
         <div class="contenedor">
+            <form name="productos">
             <?php
                 $con = conecta();
                 $sql = "SELECT * FROM productos WHERE eliminado = 0 AND status = 1 AND stock > 0
                 ORDER BY rand() LIMIT 6";
                 $res = $con->query($sql);
 
-                $cont = 0;
                 while($row = $res->fetch_array()){
                     $id = $row["id"];
                     $nombre = $row["nombre"];
@@ -33,8 +50,11 @@
                     $archivo = $row["archivo"];
             ?>
                 <div class="productoHome">
-                    <div><img class="productoHomeImagen" src="../Proyecto_backend/archivos/<?php echo $archivo?>"></div>
-                    <div class="nombreHome"><?php echo $nombre; ?></div><br>
+                    <div><img class="productoHomeImagen" src="../Proyecto_backend/archivos/<?php echo $archivo?>" 
+                    alt="<?php echo $nombre;?>" onclick="verProducto(<?php echo $id;?>);"></div>
+                    <div class="nombreHome" onclick="verProducto(<?php echo $id;?>);" style="cursor: pointer;">
+                        <?php echo $nombre; ?>
+                    </div><br>
                     <?php echo number_format($costo,2); ?><br>
                     <select id="producto_<?php echo $id; ?>" name="producto_<?php echo $id; ?>">
                         <option value="0">Selecciona</option>
@@ -45,15 +65,13 @@
                             }
                         ?>
                     </select>
-                    <input onclick="agregar(<?php echo $id; ?>);return false;" type="submit" value="Agregar"><br>
+                    <input onclick="agregar(<?php echo $id; ?>);return false;" type="submit" value="Agregar" <?php if($correoCliente == "") {?>disabled<?php }?>><br>
                     <div id="mensaje_<?php echo $id; ?>" style="color:#FF0000; font-size:11px; height: 20px"></div>
                 </div>
             <?php
-                $cont++;
-                /*if($cont >= 6)
-                    break;*/
             }
             ?>
+            </form>
         </div>
     </div>
 </body>
