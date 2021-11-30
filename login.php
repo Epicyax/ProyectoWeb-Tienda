29f22d3e78cd666247bfa38ff132bfc546731bf2
Emmanuel Garza <emmanuel-gf@hotmail.com>
2021-11-30 01:23:11 -0600

    Mensajes de error registro y login

--- login.php
+++ login.php
@@ -9,23 +9,26 @@
         <div class="wrap">
             <div class="contenedor contenedorLogin">
                 <div>
+                    <div id="err-msg-login" class="err-msg" hidden></div>
                     <form name="ingreso" method="post">
                         <h1>Iniciar sesión</h1>
-                        <input class="inputLogin" type="text" name="correoLog" id="correolog" placeholder="Correo" required/><br>
+                        <input class="inputLogin" type="email" name="correoLog" id="correolog" placeholder="Correo" required/><br>
                         <input class="inputLogin" type="password" name="passwordLog" id="passwordLog" placeholder="Contraseña" required/><br>
                         <input class="botonContacto" id="ingresar" type="submit" onclick="ingresa(); return false;" value="Ingresar"/>
-
                     </form>
                 </div>
             </div>
+            <p style="margin-top:auto; margin-bottom:auto; font-size:30px;">
+                Ó
+            </p>
             <div class="contenedor contenedorLogin">
                 <div>
+                    <div id="err-msg-registro" class="err-msg" hidden></div>
                     <form name="registro" method="post">
-                        <h3>¿No tienes cuenta?</h3>
                         <h1>Registrarse</h1>
                         <input class="inputLogin" type="text" name="nombre" id="nombre" placeholder="Nombre" required/><br>
                         <input class="inputLogin" type="text" name="apellidos" id="apellidos" placeholder="Apellidos" required/><br>
-                        <input class="inputLogin" type="text" name="correoReg" id="correoReg" placeholder="Correo" required onblur="validarCorreo();"/><br>
+                        <input class="inputLogin" type="email" name="correoReg" id="correoReg" placeholder="Correo" required onblur="validarCorreo();"/><br>
                         <input class="inputLogin" type="password" name="passwordReg" id="passwordReg" placeholder="Contraseña" required/><br>
                         <input class="botonContacto" id="registrar" type="submit" onclick="registrarse(); return false;" value="Registrarse"/>
                     </form>
